faae2a9e85ea7e20cc6847dc418bf4c500b3a8d1
Hina Hanif <hinaajutt.23@gmail.com>
2025-08-07 13:15:34 +0500

    Initial commit

--- marks_analyzer/settings.py
+++ marks_analyzer/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-1q7b2la%x&o)09$(g834@th4qbip2lm_ev*)28e+z3@#*ehq8t'
+SECRET_KEY = config('SECRET_KEY')
+DEBUG = config('DEBUG', default=False, cast=bool)
+
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
